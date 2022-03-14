5ff2f57c8156b3be1204e8d3fb2e584ad10ca248
Mark Dessain <mark.dessain@gmail.com>
2022-03-14 22:25:03 +0100

    update

--- download_binary/entrypoint.sh
+++ download_binary/entrypoint.sh
@@ -1,10 +1,10 @@
 #!/bin/sh -l
 
 echo $1 > .github_token
-gh auth login --with-token < .github_token
+/bin/gh auth login --with-token < .github_token
 rm -rf $3
 mkdir $3
-gh run download --name $2 --dir ./bin
+/bin/gh run download --name $2 --dir ./bin
 chmod +x $3/$2
 
 echo "::set-output name=binary::$3/$2"
