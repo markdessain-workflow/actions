82f9a4c953cedc498063db5f48672fa4f0a09191
Mark Dessain <mark.dessain@gmail.com>
2022-03-15 13:24:57 +0100

    update

--- checkpoint/entrypoint.sh
+++ checkpoint/entrypoint.sh
@@ -7,19 +7,14 @@ if [ ! -f "$FILE" ]; then
     echo "1" > $2.checkpoint
 fi
 
-START_DATE=$(date -r $2.checkpoint)
+START_TIME=$(date -r $2.checkpoint)
 
 OLD_NUM=$(cat $2.checkpoint)
-
-echo $OLD_NUM
-
 NEW_NUM=$(expr $OLD_NUM + 1)
+
 echo "$NEW_NUM" > $2.checkpoint
 
-END_DATE=$(date -r $2.checkpoint)
+END_TIME=$(date -r $2.checkpoint)
 
-echo $START_DATE
-echo $END_DATE
-
-echo "::set-output name=start_date::$START_DATE"
-echo "::set-output name=end_date::$END_DATE"
+echo "::set-output name=start_time::$START_TIME"
+echo "::set-output name=end_time::$END_TIME"
