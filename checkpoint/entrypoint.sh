bf161a3f5fdd5071a32a2bac0a9d4a10cc55af2b
Mark Dessain <mark.dessain@gmail.com>
2022-03-15 13:44:21 +0100

    update

--- checkpoint/entrypoint.sh
+++ checkpoint/entrypoint.sh
@@ -4,17 +4,13 @@ cd $GITHUB_WORKSPACE
 
 FILE=$2.checkpoint
 if [ ! -f "$FILE" ]; then
-    echo "1" > $2.checkpoint
+    date +%s > $2.checkpoint
 fi
 
-START_TIME=$(date -r $2.checkpoint)
+START_TIME=$(cat $2.checkpoint)
+END_TIME=$(date +%s)
 
-OLD_NUM=$(cat $2.checkpoint)
-NEW_NUM=$(expr $OLD_NUM + 1)
-
-echo "$NEW_NUM" > $2.checkpoint
-
-END_TIME=$(date -r $2.checkpoint)
+echo $END_TIME > $2.checkpoint
 
 echo "::set-output name=start_time::$START_TIME"
 echo "::set-output name=end_time::$END_TIME"
