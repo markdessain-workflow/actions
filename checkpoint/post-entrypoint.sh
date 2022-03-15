7eafbb79129acb7bca50f5014332beabe657c71b
Mark Dessain <mark.dessain@gmail.com>
2022-03-15 13:28:53 +0100

    update

--- checkpoint/post-entrypoint.sh
+++ checkpoint/post-entrypoint.sh
@@ -2,6 +2,8 @@
 
 cd $GITHUB_WORKSPACE
 
+git config --global user.email "mark.dessain@gmail.com"
+git config --global user.name "Mark Dessain"
 git add $2.checkpoint
 git commit -m "updating $2.checkpoint"
 git push
