3d9eaa5c7f5468a09c724628501414a56785bb12
Mark Dessain <mark.dessain@gmail.com>
2022-03-15 11:45:33 +0100

    update

--- checkpoint/index.js
+++ checkpoint/index.js
@@ -3,7 +3,8 @@ const github = require('@actions/github');
 
 try {
     const time = (new Date()).toTimeString();
-    core.setOutput("time", time);
+    core.setOutput("start_time", time);
+    core.setOutput("end_time", time);
 } catch (error) {
     core.setFailed(error.message);
 }
