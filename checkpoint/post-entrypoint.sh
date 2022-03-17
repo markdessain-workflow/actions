8624141a8abbe05f31ac788bb0bb6f1564799cea
Mark Dessain <mark.dessain@gmail.com>
2022-03-17 19:32:09 +0100

    update

--- checkpoint/post-entrypoint.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh -l
-
-cd $GITHUB_WORKSPACE
-
-git config --global user.email "robot@github.com"
-git config --global user.name "Robot"
-git add $2.checkpoint
-git commit -m "updating $2.checkpoint"
-git push
